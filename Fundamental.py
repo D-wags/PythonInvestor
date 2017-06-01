d2a4bc555f32f8c37400170bb47d99b32e720b2b
Drew Wagner <drew_wagner@utexas.edu>
2017-05-31 20:23:51 -0500

    annual lo/hi keeps erroring

--- Fundamental.py
+++ Fundamental.py
@@ -1,17 +1,40 @@
 import time
 from urllib.request import urlopen
 
-sp500 = ['a', 'aa', 'aabv', 'abc', 'abt', 'ace']
+Bakers = ['aqxp', 'ghdx', 'incy', 'amrn', 'blcm', 'avxs', 'hrtx', "alxp", "bcrx", "rigl", "sgmo", "anab", "aevi", "trvn"]
 
 # GET MARKET CAP
 def TickerKeyStats(ticker):
+
     try:
+        print("-----------------------------")
         sourceCode = urlopen("https://finance.yahoo.com/quote/"+ticker+"/key-statistics?p="+ticker).read()
         sourceCode = str(sourceCode)
-        pbr = sourceCode.split('Ta(end)" data-reactid="21">')[1].split('</td>')[0]
-        print("Market Cap:", str(pbr))
-    except:
-        print("Error!!")
 
-TickerKeyStats("IDRA")
+        try:
+            cap = sourceCode.split('Ta(end)" data-reactid="21">')[1].split('</td>')[0]
+            print(ticker + " market Cap:", str(cap))
+
+            price = sourceCode.split('Mb(-4px) D(ib)" data-reactid="36">')[1].split('</span>')[0]
+            print(ticker + " price:", str(price))
+
+            yrLo = sourceCode.split('Ta(end)" data-reactid="315">')[1].split('</td>')[0]
+            print(ticker + " annual low:", str(yrLo))
+
+            yrHi = sourceCode.split('Ta(end)" data-reactid="308">')[1].split('</td></tr>')[0]
+            print(ticker + " annual high:", str(yrHi))
+
+        except:
+            print(ticker + " ERROR")
+
+
+        print("")
+    except:
+        print("-----------------------------")
+        print(ticker - " - Error!!")
+
+#TickerKeyStats("idra")
+for stock in Bakers:
+    TickerKeyStats(stock)
+    time.sleep(1)
 
