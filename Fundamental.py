b3c9426a36fefc6e6d4c5badbc6d645dee049575
Drew Wagner <drew_wagner@utexas.edu>
2017-06-05 00:12:46 -0500

    Hi/Low workinggit add -A

--- Fundamental.py
+++ Fundamental.py
@@ -11,39 +11,42 @@ def TickerKeyStats(ticker):
     req = urllib.request.Request("https://finance.yahoo.com/quote/"+ticker+"/key-statistics?p="+ticker)
     with urllib.request.urlopen(req) as response:
 
-        sourceCode = response.read()
-        print("-----------------------------")
-        sourceCode = str(sourceCode)
+        try:
+            sourceCode = response.read()
+            print("-----------------------------")
+            sourceCode = str(sourceCode)
 
-        price = sourceCode.split('Mb(-4px) D(ib)" data-reactid="36">')[1].split('</span>')[0]
-        print(ticker + " price:", str(price))
+            price = sourceCode.split('Mb(-4px) D(ib)" data-reactid="36">')[1].split('</span>')[0]
+            print(ticker + " price:", str(price))
 
-        cap = sourceCode.split('Ta(end)" data-reactid="21">')[1].split('</td>')[0]
-        print(ticker + " market Cap:", str(cap))
+            cap = sourceCode.split('Ta(end)" data-reactid="21">')[1].split('</td>')[0]
+            print(ticker + " market Cap:", str(cap))
 
-        outFile = open("html_text.txt", 'w')
-        outFile.write(sourceCode)
+            outFile = open("html_text.txt", 'w')
+            outFile.write(sourceCode)
 
-        regex = 'Fz\(s\) Fw\(500\) Ta\(end\)" data-reactid="3[01][348]">'
-        yrHi = re.split(regex, sourceCode)
-        yrHi = yrHi[1].split("</td>")[0]
-        print(yrHi)
-        # a = re.split(r"\W", s)
-        # yrHi = re.split('Ta(end)" data-reactid="3[01][49]>', sourceCode)
-        # print(len(yrHi))
+            regex = 'Fz\(s\) Fw\(500\) Ta\(end\)" data-reactid="3[01][0-9]">'
+            yrHi = sourceCode.split('52 Week High')
+            yrHi = re.split(regex, yrHi[1])
+            yrHi = yrHi[1].split("</td>")[0]
+            print("52 week high: " + str(yrHi))
 
-
-        # yrHi = sourceCode.split('Fz(s) Fw(500) Ta(end)" data-reactid="3')[1].split('</td>')[0]
-        # print(ticker + " annual high:", str(yrHi[4:]))
-
-
-    # except:
-    #     print("-----------------------------")
-    #     print(ticker - " - Error!!")
-
-TickerKeyStats("idra")
+            regex = 'Fz\(s\) Fw\(500\) Ta\(end\)" data-reactid="3[12][0-9]">'
+            yrLow = sourceCode.split('52 Week Low')
+            yrLow = re.split(regex, yrLow[1])
+            yrLow = yrLow[1].split("</td>")[0]
+            print("52 week low: " + str(yrLow))
 
 
-# for stock in Bakers:
-#     TickerKeyStats(stock)
-#     time.sleep(1)
+
+
+        except:
+            print("-----------------------------")
+            print(ticker - " - Error!!")
+
+#TickerKeyStats("idra")
+
+
+for stock in Bakers:
+    TickerKeyStats(stock)
+    time.sleep(1)
