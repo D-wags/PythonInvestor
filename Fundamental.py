b0fd76e27fa56fe625057806fd38a6b1f42d44f1
Drew Wagner <drew_wagner@utexas.edu>
2017-06-07 11:53:45 -0500

    exception handling

--- Fundamental.py
+++ Fundamental.py
@@ -1,3 +1,4 @@
+
 import re
 import time
 import urllib.request
@@ -5,22 +6,22 @@ import urllib.request
 Bakers = ['aqxp', 'ghdx', 'incy', 'amrn', 'blcm', 'bcrx', 'avxs', 'rigl', 'sgmo', 'anab', 'trvn']
 
 # GET MARKET DATA
-def TickerKeyStats(ticker):
+def TickerStats(ticker):
 
-    #try:
-    req = urllib.request.Request("https://finance.yahoo.com/quote/"+ticker+"/key-statistics?p="+ticker)
-    with urllib.request.urlopen(req) as response:
+    try:
+        req = urllib.request.Request("https://finance.yahoo.com/quote/"+ticker+"/key-statistics?p="+ticker)
+        with urllib.request.urlopen(req) as response:
 
-        try:
+
             sourceCode = response.read()
             print("-----------------------------")
             sourceCode = str(sourceCode)
 
             price = sourceCode.split('Mb(-4px) D(ib)" data-reactid="36">')[1].split('</span>')[0]
-            print(ticker + " price:", str(price))
+            print("{} price: {}".format(ticker,str(price)))
 
             cap = sourceCode.split('Ta(end)" data-reactid="21">')[1].split('</td>')[0]
-            print(ticker + " market Cap:", str(cap))
+            print("{} market Cap: {}".format(ticker, str(cap)))
 
             outFile = open("html_text.txt", 'w')
             outFile.write(sourceCode)
@@ -29,24 +30,20 @@ def TickerKeyStats(ticker):
             yrHi = sourceCode.split('52 Week High')
             yrHi = re.split(regex, yrHi[1])
             yrHi = yrHi[1].split("</td>")[0]
-            print("52 week high: " + str(yrHi))
+            print("52 week high: {}".format(str(yrHi)))
 
             regex = 'Fz\(s\) Fw\(500\) Ta\(end\)" data-reactid="3[12][0-9]">'
             yrLow = sourceCode.split('52 Week Low')
             yrLow = re.split(regex, yrLow[1])
             yrLow = yrLow[1].split("</td>")[0]
-            print("52 week low: " + str(yrLow))
+            print("52 week low: {}".format(str(yrLow)))
 
+    except Exception as inst:
+        print("-----------------------------")
+        print(ticker - " - Error!!")
+        print(inst)
 
-
-
-        except:
-            print("-----------------------------")
-            print(ticker - " - Error!!")
-
-#TickerKeyStats("idra")
-
-
+#TickerStats("idra")
 for stock in Bakers:
-    TickerKeyStats(stock)
+    TickerStats(stock)
     time.sleep(1)
